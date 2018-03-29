80aaa5185a97e063e2979023aaf4b8673604e7ee
BuildTools <unconfigured@null.spigotmc.org>
2018-03-29 17:54:27 -0400

    first commit part2

---
+++ app/src/main/java/com/example/andrewclark/lab6_clark/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.andrewclark.csci490_lab_04_clark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andrewclark.lab6_clark.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
